9da8d74b85404af7de7cf5a894e5ee84cfd67928
Sinches-Hugo-Ivan <hugo.ivan.sinches@gmail.com>
2021-11-25 14:04:28 -0300

    Crud 1

---
+++ src/data/products_db.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// module.exports = {
+//     products : JSON.parse(fs.readFileSync(path.join( __dirname,'productsDataBase.json'),'utf-8')),
+//     guardar : producto => fs.writeFileSync(path.join(__dirname, 'productsDataBase.json'),JSON.stringify(producto,null,2),'utf-8')
+// }
